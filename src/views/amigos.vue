c5719db8cd171ffedf3f1680fe3a334ef94b4d47
Escandon <aleskndon@hotmail.com>
2020-09-13 17:17:48 -0500

    Views that router handle to make a SPA

---
+++ src/views/amigos.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="columns mt-2">
+    <div class="columns is-centered">
+      <div class="column is-narrow">
+        <table class="table">
+          <thead>
+            <tr>
+              <th><abbr title="Position">Nombre</abbr></th>
+              <th>Email</th>
+              <th><abbr title="Played">Mood</abbr></th>
+              <th><abbr title="Won">Phone</abbr></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="amigo in amigos" :key="amigo.id">
+              <th> {{ amigo.name }}</th>
+              <th> {{ amigo.email }}</th>
+              <th> {{ amigo.mood }}</th>
+              <th> {{ amigo.phone }}</th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "amigos",
+  computed: {
+    amigos() {
+      return this.$store.getters.getterAmigos;
+    },
+  },
+    created() {
+      this.$store.dispatch('FETCH_AMIGOS')
+    }
+};
+</script>
+
+<style>
+.table {
+  margin-left: auto;
+  margin-right: auto;
+  /* display: flex; */
+  justify-content: center;
+}
+#size {
+  width: 100%;
+}
+@media screen and (min-width: 960px) {
+  #size {
+    width: 70%;
+  }
+}
+</style>
---
+++ src/views/fiestas.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <FormNewParty :show="mostrar" @cerrar="closeModal" />
+    <FormEditParty
+      :showEditar="editShow"
+      @cerrarE="closeEdit"
+      :fiesta="propFiesta"
+    />
+    <!-- <div class="panel-block"> -->
+    <div class="columns mt-1">
+      <div class="column is-three-quarters">
+        <p class="control has-icons-left">
+          <input class="input is-primary" type="text" placeholder="Buscar" />
+          <span class="icon is-left">
+            <i class="fas fa-search" aria-hidden="true"></i>
+          </span>
+        </p>
+      </div>
+      <div class="column level">
+        <div class="level level-left">
+          <div class="level-item">
+            <button class="button is-primary is-light" @click="mostrar = true">
+              <p class="mr-3">nuevo</p>
+              <fa-icon icon="plus" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end layout -->
+    <div v-for="fiesta in partys" :key="fiesta.id" class="ml-3 mr-4">
+      <div class="columns is-mobile">
+        <a class="column is-mobile is-9" @click="editFiesta(fiesta)">
+          <span class="panel-icon">
+            <fa-icon icon="home" />
+            <i class="fas fa-book" aria-hidden="true"></i>
+          </span>
+          {{ fiesta.name }}
+        </a>
+        <div class="column">
+          <button class="button is-link is-light is-rounded" @click="lista(fiesta)">
+            <fa-icon icon="plus" />
+          </button>
+        </div>
+      </div>
+    </div>
+    <ListThings 
+    :verThings="verModalThings"
+    :fiesta="propFiesta"
+    @closeThings="verModalThings = false"
+    />
+  </div>
+</template>
+
+<script>
+import FormNewParty from "@/components/FormNewParty";
+import FormEditParty from "@/components/FormEditParty";
+import ListThings from "@/components/ListThings";
+export default {
+  data: () => {
+    return {
+      mostrar: false,
+      editShow: false,
+      verModalThings: false,
+      propFiesta: "",
+    };
+  },
+  components: {
+    FormNewParty,
+    FormEditParty,
+    ListThings,
+  },
+  name: "fiestas",
+  computed: {
+    partys() {
+      return this.$store.getters.fiestas;
+    },
+  },
+  created() {
+    this.$store.dispatch("fetch_party");
+  },
+  methods: {
+    openModal() {
+      this.mostrar = true;
+    },
+    closeModal(e) {
+      this.mostrar = false;
+    },
+    closeEdit(e) {
+      this.editShow = false;
+    },
+    editFiesta(e) {
+      this.propFiesta = e;
+      this.editShow = true;
+    },
+
+    lista(f){
+      console.log('i suppost to display list');
+      this.propFiesta = f
+      this.verModalThings = true
+    }
+  },
+};
+</script>
+
+<style></style>
