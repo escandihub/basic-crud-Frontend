380ac924d9f6ec48f07ebe727b332f7655a733ac
Escandon <aleskndon@hotmail.com>
2020-09-13 17:23:08 -0500

    other things that vue make this very well

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import './icons.js'
+
+require('@/assets/main.scss');
+
+Vue.config.productionTip = false
+new Vue({
+  router,
+  store,
+  render: function (h) { return h(App) }
+}).$mount('#app')
