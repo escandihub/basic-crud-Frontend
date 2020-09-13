5f4fec7f5033903ade164d7bc3897bd20b50d3ba
Escandon <aleskndon@hotmail.com>
2020-09-13 17:19:41 -0500

    no ended but will make a cool usign vuex

---
+++ src/components/notification.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="notification is-success" v-show="false">
+    <button class="delete"></button>
+    {{ 'notif.mensaje' }}
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex"
+export default {
+  created() {
+    setTimeout(() => {
+       
+    })
+  },
+   computed: {
+     notif(){
+       
+       return this.$store.getters.notificar
+     }
+   }
+};
+</script>
+
+<style></style>
