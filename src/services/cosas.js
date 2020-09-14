ebec64984ac404f82caf28be6aeb07b735086d7e
Escandon <aleskndon@hotmail.com>
2020-09-14 10:25:05 -0500

    add a params to send idParty

--- src/services/cosas.js
+++ src/services/cosas.js
@@ -1,8 +1,12 @@
 import base from './base';
 
-export function getThings() {
+export function getThings(id_fiesta) {
   return new Promise((response, reject) => {
-    base().get('/things').then(res => {
+    base().get('/things',   {
+      params: {
+        idParty: id_fiesta
+      }
+    }).then(res => {
       response(res)
     }).catch(err => {
       reject(err)
